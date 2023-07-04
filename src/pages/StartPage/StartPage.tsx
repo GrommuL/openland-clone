5b68a46609f00c16320327f943052dc64927f677
GrommuL <miketsvg@gmail.com>
2023-07-04 13:11:42 +0600

    Добавил стартовую страницу

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -1,20 +1,26 @@
-import Logo from '@/assets/Logo.png'
-import style from './StartPage.module.scss'
 import { Link } from 'react-router-dom'
+import Logo from '@/assets/Logo.png'
+import cn from 'classnames'
+import style from './StartPage.module.scss'
 
 export const StartPage = () => {
 	return (
-		<main>
-			<div>
-				<div>
-					<img src={Logo} alt='Logo' />
-					<div>
-						<h1>Openland</h1>
-						<p>Modern social network built for you, not advertisers</p>
+		<main className={cn(style.startPage)}>
+			<div className={cn(style.content)}>
+				<div className={cn(style.box)}>
+					<img className={cn(style.logo)} src={Logo} alt='Logo' />
+					<div className={cn(style.info)}>
+						<h1 className={cn(style.title)}>Openland</h1>
+						<p className={cn(style.description)}>
+							Modern social network
+							<br />
+							built for you, not advertisers
+						</p>
 					</div>
 				</div>
-				<div>
-					<Link to='/'></Link>
+				<div className={cn(style.buttons)}>
+					<Link to='/'>Login</Link>
+					<Link to='/'>Register</Link>
 				</div>
 			</div>
 		</main>
