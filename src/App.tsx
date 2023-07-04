7f856bb4317cdcc47092b59378abfbe38a6bb5a3
GrommuL <miketsvg@gmail.com>
2023-07-04 15:55:47 +0600

    Добавил страницу авторизации

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { RegisterPage } from '@/pages/RegisterPage'
-import { StartPage } from './pages/StartPage'
+import { StartPage } from '@/pages/StartPage'
+import { LoginPage } from '@/pages/LoginPage'
 
 export const App = () => {
 	return (
 		<main>
 			<RegisterPage />
 			{/* <StartPage /> */}
+			{/* <LoginPage /> */}
 		</main>
 	)
 }
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,28 @@
 import cn from 'classnames'
 import style from './LoginPage.module.scss'
+import Logo from '@/assets/Logo.png'
+import { Heading } from '@/components/UI/Heading'
+import { Button } from '@/components/UI/Button'
 
 export const LoginPage = () => {
-	return <div>LoginPage</div>
+	return (
+		<main className={cn(style.loginPage)}>
+			<form className={cn(style.content)}>
+				<img className={cn(style.logo)} src={Logo} alt='Logo' />
+				<Heading
+					title='Welcome back!'
+					descirption='Sign in now and gain access to exclusive content!'
+				/>
+				<div className={cn(style.inputsContainer)}>
+					<input className={cn(style.input)} type='email' placeholder='Email' />
+					<input
+						className={cn(style.input)}
+						type='password'
+						placeholder='Password'
+					/>
+				</div>
+				<Button label='Sign in' type='submit' />
+			</form>
+		</main>
+	)
 }
