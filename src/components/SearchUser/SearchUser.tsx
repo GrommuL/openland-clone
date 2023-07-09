19f9b21d6f6c2fd252443e2ca0e0e499e6dffcd0
GrommuL <miketsvg@gmail.com>
2023-07-09 11:36:08 +0600

    Реализовал добавление пользователя после поиска в активные чаты

--- src/components/SearchUser/SearchUser.tsx
+++ src/components/SearchUser/SearchUser.tsx
@@ -2,11 +2,22 @@ import cn from 'classnames'
 import style from './SearchUser.module.scss'
 import { CloseIcon } from '../UI/Icons/CloseIcon'
 import { useState } from 'react'
-import { DocumentData, collection, getDocs } from 'firebase/firestore'
+import {
+	DocumentData,
+	collection,
+	doc,
+	getDoc,
+	getDocs,
+	serverTimestamp,
+	setDoc,
+	updateDoc
+} from 'firebase/firestore'
 import { db } from '@/config/firebase'
 import { SearchUserItem } from '../SearchUserItem/SearchUserItem'
+import { useAppSelector } from '@/utils/hooks/useAppSelector'
 
 export const SearchUser = () => {
+	const currentUser = useAppSelector((state) => state.user.currentUser)
 	const [searchValue, setSearchValue] = useState('')
 	const [user, setUser] = useState<DocumentData[] | null>(null)
 
@@ -32,6 +43,45 @@ export const SearchUser = () => {
 			setUser(selectedUser)
 		}
 		setSearchValue('')
+	}
+
+	const handleSelect = async (id: string) => {
+		const selectedSearchUser = user?.find((useritem) => {
+			return useritem.data().uid === id
+		})
+		const combinedId =
+			currentUser.id > selectedSearchUser?.data().uid
+				? currentUser.id + selectedSearchUser?.data().uid
+				: selectedSearchUser?.data().uid + currentUser.id
+
+		try {
+			const response = await getDoc(doc(db, 'chats', combinedId))
+			if (!response.exists()) {
+				await setDoc(doc(db, 'chats', combinedId), { messages: [] })
+
+				await updateDoc(doc(db, 'userChats', currentUser.id), {
+					[combinedId + '.userInfo']: {
+						uid: selectedSearchUser?.data().uid,
+						firstName: selectedSearchUser?.data().firstName,
+						lastName: selectedSearchUser?.data().lastName,
+						photoURL: selectedSearchUser?.data().photoURL
+					},
+					[combinedId + '.date']: serverTimestamp()
+				})
+				await updateDoc(doc(db, 'userChats', selectedSearchUser?.data().uid), {
+					[combinedId + '.userInfo']: {
+						uid: currentUser.id,
+						firstName: currentUser.firstName,
+						lastName: currentUser.lastName,
+						photoURL: currentUser.avatar
+					},
+					[combinedId + '.date']: serverTimestamp()
+				})
+			}
+			setUser(null)
+		} catch (error) {
+			console.log(error)
+		}
 	}
 
 	const handleKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -60,6 +110,7 @@ export const SearchUser = () => {
 			{user &&
 				user.map((searchUser) => (
 					<SearchUserItem
+						onClick={() => handleSelect(searchUser.data().uid)}
 						user={searchUser.data()}
 						key={searchUser.data().uid}
 					/>
