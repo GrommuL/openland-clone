3a232e5586105e2bf968e7ef1158bdd24b3e5ed8
GrommuL <miketsvg@gmail.com>
2023-07-11 11:42:05 +0600

    Рефакторинг маршрутизации

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { App } from '@/App.tsx'
-import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import store, { persistor } from '@/redux/store.ts'
@@ -11,12 +10,10 @@ import '@/styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<Provider store={store}>
-				<PersistGate loading={null} persistor={persistor}>
-					<App />
-				</PersistGate>
-			</Provider>
-		</BrowserRouter>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
+		</Provider>
 	</React.StrictMode>
 )
---
+++ src/utils/router/index.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from 'react-router-dom'
+import { PrivateRoutes } from './PrivateRoutes'
+import {
+	StartPage,
+	RegisterPage,
+	LoginPage,
+	HomePage,
+	NotFoundPage,
+	UserPage
+} from '@/pages'
+
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <PrivateRoutes />,
+		children: [
+			{ path: '', element: <HomePage /> },
+			{ path: 'user', element: <UserPage /> }
+		]
+	},
+	{ path: '/start', element: <StartPage /> },
+	{ path: '/login', element: <LoginPage /> },
+	{ path: '/register', element: <RegisterPage /> },
+	{ path: '/*', element: <NotFoundPage /> }
+])
