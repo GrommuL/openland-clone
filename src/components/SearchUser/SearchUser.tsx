7cba9b7f4be8e9a518a5b2f9cdaf57d086f2f934
GrommuL <miketsvg@gmail.com>
2023-07-11 13:58:23 +0600

    Вынес логику поиска и добавления пользователя в кастомный хук useSearchUser

--- src/components/SearchUser/SearchUser.tsx
+++ src/components/SearchUser/SearchUser.tsx
@@ -1,86 +1,12 @@
 import cn from 'classnames'
 import style from './SearchUser.module.scss'
 import { CloseIcon } from '../UI/Icons/CloseIcon'
-import { useState } from 'react'
-import {
-	DocumentData,
-	collection,
-	doc,
-	getDoc,
-	getDocs,
-	serverTimestamp,
-	setDoc,
-	updateDoc
-} from 'firebase/firestore'
-import { db } from '@/config/firebase'
 import { SearchUserItem } from '../SearchUserItem/SearchUserItem'
-import { useAppSelector } from '@/utils/hooks/useAppSelector'
+import { useSearchUser } from '@/utils/hooks/useSearchUser'
 
 export const SearchUser = () => {
-	const currentUser = useAppSelector((state) => state.user.currentUser)
-	const [searchValue, setSearchValue] = useState('')
-	const [user, setUser] = useState<DocumentData[] | null>(null)
-
-	const handleSearch = async () => {
-		const users = collection(db, 'users')
-		const data = await getDocs(users)
-		const selectedUser = data?.docs.filter((item) => {
-			return (
-				item
-					.data()
-					.firstName.toLowerCase()
-					.includes(searchValue.toLocaleLowerCase()) ||
-				item
-					.data()
-					.lastName.toLowerCase()
-					.includes(searchValue.toLocaleLowerCase())
-			)
-		})
-
-		if (selectedUser) {
-			setUser(selectedUser)
-		}
-		setSearchValue('')
-	}
-
-	const handleSelect = async (id: string) => {
-		const selectedSearchUser = user?.find((useritem) => {
-			return useritem.data().uid === id
-		})
-		const combinedId =
-			currentUser.id > selectedSearchUser?.data().uid
-				? currentUser.id + selectedSearchUser?.data().uid
-				: selectedSearchUser?.data().uid + currentUser.id
-
-		try {
-			const response = await getDoc(doc(db, 'chats', combinedId))
-			if (!response.exists()) {
-				await setDoc(doc(db, 'chats', combinedId), { messages: [] })
-
-				await updateDoc(doc(db, 'userChats', currentUser.id), {
-					[combinedId + '.userInfo']: {
-						uid: selectedSearchUser?.data().uid,
-						firstName: selectedSearchUser?.data().firstName,
-						lastName: selectedSearchUser?.data().lastName,
-						photoURL: selectedSearchUser?.data().photoURL
-					},
-					[combinedId + '.date']: serverTimestamp()
-				})
-				await updateDoc(doc(db, 'userChats', selectedSearchUser?.data().uid), {
-					[combinedId + '.userInfo']: {
-						uid: currentUser.id,
-						firstName: currentUser.firstName,
-						lastName: currentUser.lastName,
-						photoURL: currentUser.avatar
-					},
-					[combinedId + '.date']: serverTimestamp()
-				})
-			}
-			setUser(null)
-		} catch (error) {
-			console.log(error)
-		}
-	}
+	const { handleSearch, handleSelect, searchValue, setSearchValue, user } =
+		useSearchUser()
 
 	const handleKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
 		e.code === 'Enter' && handleSearch()
