6a41d90d72468e5089453fb6b3e2335bd0d30523
GrommuL <miketsvg@gmail.com>
2023-07-11 12:53:38 +0600

    Добавил отображение пустого чата при отсутствии chatId

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,8 +4,11 @@ import style from './HomePage.module.scss'
 import { SearchUser } from '@/components/SearchUser'
 import { ChatList } from '@/components/ChatList'
 import { ChatContent } from '@/components/ChatContent'
+import { useAppSelector } from '@/utils/hooks/useAppSelector'
+import Logo from '@/assets/Logo.png'
 
 export const HomePage = () => {
+	const chatId = useAppSelector((state) => state.chat.chatId)
 	return (
 		<AppContainer>
 			<div className={cn(style.appSidebar)}>
@@ -13,7 +16,13 @@ export const HomePage = () => {
 				<SearchUser />
 				<ChatList />
 			</div>
-			<ChatContent />
+			{chatId.length ? (
+				<ChatContent />
+			) : (
+				<div className={cn(style.emptyChat)}>
+					<img src={Logo} alt='' />
+				</div>
+			)}
 		</AppContainer>
 	)
 }
